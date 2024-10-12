6d80929e315749f2a1c2fe7fd5c0dc67556cb9be
Riad Ashrafov <46532875+Slater8242@users.noreply.github.com>
2024-10-12 21:53:51 +0000

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { Box, Container, CssBaseline } from '@mui/material'
 import { Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux';
 import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import ProductDetails from './components/ProductDetails'
+import store from './store';
 
 function App() {
 
   return (
-    <Box sx={{
-      minHeight: "100vh",
-      display:"flex",
-      flexDirection:"column"
-    }}>
-      <CssBaseline />
-      <Header/>
-      <Container>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
-            <Route path='/contact' element={<Contact />}/>
-            <Route path="/products/:id" element={<ProductDetails />} />
-          </Routes>
-      </Container>
-      <Footer/>
-    </Box>
+    <Provider store={store}>
+      <Box sx={{
+        minHeight: "100vh",
+        display:"flex",
+        flexDirection:"column"
+      }}>
+        <CssBaseline />
+        <Header/>
+        <Container>
+            <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/about' element={<About />}/>
+              <Route path='/contact' element={<Contact />}/>
+              <Route path="/product/:id" element={<ProductDetails />} />
+            </Routes>
+        </Container>
+        <Footer/>
+      </Box>
+    </Provider>
   )
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Box, Typography,  Card, CardMedia, CardContent, CardActions, Button, Rating, CircularProgress, Tooltip } from '@mui/material';
 import Grid from "@mui/material/Grid2"
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../features/products/productsSlice';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.items);
+  const productStatus = useSelector((state) => state.products.status);
+  const error = useSelector((state) => state.products.error);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-        .then((res) => res.json())
-        .then((data) => {
-          setProducts(data);
-          setLoading(false);
-        })
-        .catch(() => setLoading(false));
-  }, []);
+    if (productStatus === 'idle') {
+      dispatch(fetchProducts());
+    }
+  }, [productStatus, dispatch]);
 
-  if (loading) {
+  if (productStatus === 'loading') {
     return (
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
         <CircularProgress />
@@ -25,19 +25,19 @@ const Products = () => {
     );
   }
 
-  const noProducts = 
-    <Box sx={{ textAlign: 'center', mt: 5 }}>
-      <Typography variant="h6">No products available</Typography>
-    </Box>
+  if (productStatus === 'failed') {
+    return (
+      <Box sx={{ textAlign: 'center', mt: 5 }}>
+        <Typography variant="h6">No products available</Typography>
+      </Box>
+    )
+  } 
 
   return (
     <Box sx={{ p: 3 }}>
-      <Typography variant="h4" gutterBottom>
+      <Typography variant="h3" gutterBottom sx={{ textAlign: 'center', fontWeight: 'bold', color: 'primary.main' }}>
         Our Products
       </Typography>
-      {products.length == 0 ? 
-        noProducts 
-        :
         <Grid container spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} size={{xs: 12, md: 4, sm:6}}>
@@ -50,12 +50,12 @@ const Products = () => {
                 sx={{ objectFit: 'contain'}}
               />
               <CardContent sx={{ flexGrow: 1 }}>
-                <Tooltip title={product.title.length > 47 ? product.title : null}>
+                <Tooltip title={product.title.length > 47 ? product.title : null} enterNextDelay={500} leaveDelay={200}>
                   <Typography gutterBottom variant="h6" sx={{ minHeight: 60 }}>
                     {product.title.length > 50 ? `${product.title.substring(0, 47)}...` : product.title}
                   </Typography>
                 </Tooltip>
-                <Tooltip title={product.description.length > 97 ? product.description : null}>
+                <Tooltip title={product.description.length > 97 ? product.description : null} enterNextDelay={500} leaveDelay={200}>
                   <Typography variant="body2" color="text.secondary" sx={{ minHeight: 80 }}>
                     {product.description.length > 100 ? `${product.description.substring(0, 97)}...` : product.description}
                   </Typography>
@@ -79,8 +79,8 @@ const Products = () => {
                 <Button
                   size="small"
                   component={Link}
-                  to={`/products/${product.id}`}
-                  variant="contained"
+                  to={`/product/${product.id}`}
+                  // variant="contained"
                   color="primary"
                 >
                   View Details
@@ -90,7 +90,6 @@ const Products = () => {
           </Grid>
         ))}
       </Grid>
-      }
     </Box>
   );
 };
